c25949653cd5d458a6f7ee871123dc607d431ee1
vla5924 <vla5924@gmail.com>
2020-11-27 17:52:56 +0300

    some view improvements

--- 01_spline/src/splinechart.cpp
+++ 01_spline/src/splinechart.cpp
@@ -1,7 +1,7 @@
 #include "splinechart.h"
 
 constexpr double SPLINECHART_STEPS = 100;
-constexpr double SPLINECHART_GAPFACTOR = 0.2;
+constexpr double SPLINECHART_GAPFACTOR = 0.25;
 
 SplineChart::SplineChart(QGraphicsItem  *parent) : QtCharts::QChart(parent)
 {
@@ -11,6 +11,7 @@ SplineChart::SplineChart(QGraphicsItem  *parent) : QtCharts::QChart(parent)
     p_spline_series->setColor(QColorConstants::Blue);
     p_points_series = new QtCharts::QScatterSeries();
     p_points_series->setColor(QColorConstants::DarkBlue);
+    p_points_series->setMarkerSize(10);
     p_axis_h_series = new QtCharts::QLineSeries();
     p_axis_h_series->setPen(QColor(0, 0, 0));
     p_axis_h_series->setOpacity(0.3);
