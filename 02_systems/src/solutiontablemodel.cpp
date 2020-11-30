fd416031964da8c2c13199f1d5040610ec4823ea
vla5924 <vla5924@gmail.com>
2020-11-30 17:41:41 +0300

    Solution struct for table model

--- 02_systems/src/solutiontablemodel.cpp
+++ 02_systems/src/solutiontablemodel.cpp
@@ -1,10 +1,10 @@
 #include "solutiontablemodel.h"
 
-SolutionTableModel::SolutionTableModel(const std::vector<Column>& solutions,
-                                       const std::vector<double>& durations, QObject *parent)
-    : QAbstractTableModel(parent), m_solutions(solutions), m_durations(durations)
+SolutionTableModel::SolutionTableModel(const std::vector<Solution>& solutions,
+                                       QObject *parent)
+    : QAbstractTableModel(parent), m_solutions(solutions)
 {
-    m_eq_count = static_cast<int>(m_solutions[0].size());
+    m_eq_count = static_cast<int>(m_solutions[0].column.size());
 }
 
 int SolutionTableModel::rowCount(const QModelIndex &) const
@@ -14,7 +14,7 @@ int SolutionTableModel::rowCount(const QModelIndex &) const
 
 int SolutionTableModel::columnCount(const QModelIndex &) const
 {
-    return static_cast<int>(m_durations.size());
+    return static_cast<int>(m_solutions.size());
 }
 
 QVariant SolutionTableModel::data(const QModelIndex &index, int role) const
@@ -23,7 +23,7 @@ QVariant SolutionTableModel::data(const QModelIndex &index, int role) const
         return QVariant();
     int row = index.row();
     int column = index.column();
-    return row >= m_eq_count ? m_durations[column] : m_solutions[column][row];
+    return row >= m_eq_count ? m_solutions[column].duration : m_solutions[column].column[row];
 }
 
 QVariant SolutionTableModel::headerData(int column, Qt::Orientation orientation, int role) const
@@ -32,7 +32,17 @@ QVariant SolutionTableModel::headerData(int column, Qt::Orientation orientation,
         return QVariant();
     if (orientation == Qt::Vertical)
         return column >= m_eq_count ? QString("Duration") : "x" + QString::fromStdString(std::to_string(column + 1));
-    if (column == 0)
-        return "Gauss";
-    return "method name";
+    if (column == GaussMethod)
+        return "Gauss method";
+    if (column == KramerMethod)
+        return "Kramer method";
+    if (column == SeidelMethod)
+        return "Seidel method";
+    if (column == SimpleIterationMethod)
+        return "Simple iteration method";
+    if (column == UpperRelaxationMethod)
+        return "Upper relaxation method";
+    if (column == JacobiMethod)
+        return "Jacobi method";
+    return "Unknown method";
 }
