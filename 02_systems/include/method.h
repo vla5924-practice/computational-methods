1cb5ec00b0a4602cee561d1f8ada58303c16a70e
vla5924 <vla5924@gmail.com>
2020-11-30 17:41:16 +0300

    Method enum globalized

---
+++ 02_systems/include/method.h
@@ -0,0 +1,14 @@
+#ifndef METHOD_H
+#define METHOD_H
+
+enum Method
+{
+    GaussMethod,
+    KramerMethod,
+    SeidelMethod,
+    SimpleIterationMethod,
+    UpperRelaxationMethod,
+    JacobiMethod
+};
+
+#endif // METHOD_H
