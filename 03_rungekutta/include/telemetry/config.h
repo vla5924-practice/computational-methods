0b1517ddced234364924294bc9a84dca2aa84fd1
vla5924 <vla5924@gmail.com>
2021-02-27 17:52:58 +0300

    Telemetry fixes

--- 03_rungekutta/include/telemetry/config.h
+++ 03_rungekutta/include/telemetry/config.h
@@ -1,7 +1,10 @@
 #pragma once
 
+// Comment next line to disable telemetry
 #define ENABLE_TELEMETRY
 
+// Define base URL for telemetry data sending
 #define TELEMETRY_URL "https://mvla.ru/experiments/egamov-telemetry/"
 
+// Define string to be appended to telemetry data JSON object as 'token' field
 #define TELEMETRY_TOKEN ""
--- 03_rungekutta/include/telemetry/inc.h
+++ 03_rungekutta/include/telemetry/inc.h
@@ -6,8 +6,12 @@
 
 #include "telemetry/service.h"
 TelemetryService qTelemetryService(TELEMETRY_TOKEN);
-
 #define SEND_TELEMETRY(QJSONOBJECT) qTelemetryService.sendData(QJSONOBJECT)
 #define SEND_TELEMETRY_ACTION(ACTION) qTelemetryService.sendData(QJsonObject{{ "action", (ACTION) }})
+
+#else
+
+#define SEND_TELEMETRY(QJSONOBJECT)
+#define SEND_TELEMETRY_ACTION(ACTION)
 
 #endif
