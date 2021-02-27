a194e2eb9f522bba7315a15e77ddf1d19177813f
vla5924 <vla5924@gmail.com>
2021-02-27 19:28:35 +0300

    RungeKutta solver improvements

--- 03_rungekutta/src/rkmethodsolver.cpp
+++ 03_rungekutta/src/rkmethodsolver.cpp
@@ -2,8 +2,6 @@
 
 #include <stdexcept>
 
-constexpr double NO_PARAM = 0; // TODO: Impl f1 and f2 use as f3 in RKMethodSolver::solve
-
 RKMethodSolver::RKMethodSolver(AbstractEquationSystem *system)
 {
     m_system = system;
@@ -30,23 +28,25 @@ std::vector<std::array<double, 4>> RKMethodSolver::solve(double a, double b, int
 
     for (int i = 1; i <= n; i++)
     {
-        K[0][0] = m_system->f1(NO_PARAM, y_prev, NO_PARAM);
-        K[0][1] = m_system->f1(NO_PARAM, y_prev + h / 2, NO_PARAM);
-        K[0][2] = m_system->f1(NO_PARAM, y_prev + h * K[0][1] / 2, NO_PARAM);
-        K[0][3] = m_system->f1(NO_PARAM, y_prev + h * K[0][2], NO_PARAM);
-        x = x_prev + (K[0][0] + 2 * K[0][1] + 2 * K[0][2] + K[0][3]) * h / 6;
+        K[0][0] = h * m_system->f1(x_prev, y_prev, z_prev);
+        K[1][0] = h * m_system->f2(x_prev, y_prev, z_prev);
+        K[2][0] = h * m_system->f3(x_prev, y_prev, z_prev);
 
-        K[1][0] = m_system->f2(x_prev, NO_PARAM, NO_PARAM);
-        K[1][1] = m_system->f2(x_prev + h / 2, NO_PARAM, NO_PARAM);
-        K[1][2] = m_system->f2(x_prev + h * K[1][1] / 2, NO_PARAM, NO_PARAM);
-        K[1][3] = m_system->f2(x_prev + h * K[1][2], NO_PARAM, NO_PARAM);
-        y = y_prev + (K[1][0] + 2 * K[1][1] + 2 * K[1][2] + K[1][3]) * h / 6;
+        K[0][1] = h * m_system->f1(x_prev + K[0][0] / 2, y_prev + K[1][0], z_prev + K[2][0] / 2);
+        K[1][1] = h * m_system->f2(x_prev + K[0][0] / 2, y_prev + K[1][0], z_prev + K[2][0] / 2);
+        K[2][1] = h * m_system->f3(x_prev + K[0][0], y_prev + K[1][0], z_prev + K[2][0] / 2);
 
-        K[2][0] = m_system->f3(x_prev, y_prev, z_prev);
-        K[2][1] = m_system->f3(x_prev + h / 2, y_prev + h * K[1][0] / 2, z_prev + h * K[2][0] / 2);
-        K[2][2] = m_system->f3(x_prev + h * K[0][1] / 2, y_prev + h * K[1][1] / 2, z_prev + h * K[2][1] / 2);
-        K[2][3] = m_system->f3(x_prev + h * K[0][2], y_prev + h * K[1][2] / 2, z_prev + h * K[2][2] / 2);
-        z = z_prev + (K[2][0] + 2 * K[2][1] + 2 * K[2][2] + K[2][3]) * h / 6;
+        K[0][2] = h * m_system->f1(x_prev + K[0][1] / 2, y_prev + K[1][1] / 2, z_prev + K[2][1] / 2);
+        K[1][2] = h * m_system->f2(x_prev + K[0][1] / 2, y_prev + K[1][1] / 2, z_prev + K[2][1] / 2);
+        K[2][2] = h * m_system->f3(x_prev + K[0][1], y_prev + K[1][1] / 2, z_prev + K[2][1] / 2);
+
+        K[0][3] = h * m_system->f1(x_prev + K[0][2], y_prev + K[1][2], z_prev + K[2][2]);
+        K[1][3] = h * m_system->f2(x_prev + K[0][2], y_prev + K[1][2], z_prev + K[2][2]);
+        K[2][3] = h * m_system->f3(x_prev + K[0][2], y_prev + K[1][2], z_prev + K[2][2]);
+
+        x = x_prev + (K[0][0] + 2 * K[0][1] + 2 * K[0][2] + K[0][3]) / 6;
+        y = y_prev + (K[1][0] + 2 * K[1][1] + 2 * K[1][2] + K[1][3]) / 6;
+        z = z_prev + (K[2][0] + 2 * K[2][1] + 2 * K[2][2] + K[2][3]) / 6;
 
         result.push_back({ x, y, z, t });
         t += h;
