c8eab16fa54fdbfb4dfae690399322d2c911e76c
vla5924 <vla5924@gmail.com>
2021-02-28 14:03:57 +0300

    Telemetry memleak fix

--- 03_rungekutta/include/telemetry/service.h
+++ 03_rungekutta/include/telemetry/service.h
@@ -2,14 +2,16 @@
 
 #include <QString>
 #include <QJsonObject>
+#include <QtNetwork/QNetworkAccessManager>
 
 class TelemetryService
 {
+    QNetworkAccessManager* m_manager;
     QString m_token;
 
 public:
     TelemetryService(const QString& token = "");
-    ~TelemetryService() = default;
+    ~TelemetryService();
 
     void sendData(const QJsonObject& data);
 };
